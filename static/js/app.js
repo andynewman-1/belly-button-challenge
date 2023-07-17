2c9784be847ce25e1e6a672b95ae9d699ef3ac1e
Andy Newman <124229773+andynewman-1@users.noreply.github.com>
2023-07-17 21:37:28 +0930

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -2,7 +2,7 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
 // Fetch the JSON data and console log it
-d3.json(url).then(function(data) {
+d3.json("data.json").then(function(data) {
   console.log(data);});
 
 // Initialize the dashboard at start up 
